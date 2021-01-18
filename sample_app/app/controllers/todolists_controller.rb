fbc3bed15ac9dd34dae60f0793705d74c42c38af
EC2 Default User <ec2-user@ip-172-31-58-87.ec2.internal>
2021-01-18 01:15:10 +0000

    [Start]2-8

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -26,9 +26,16 @@ class TodolistsController < ApplicationController
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
+
+  # def destroy
+  #   list = List.find(params[:id])
+  #   list.destroy
+  #   redirect_to todolists_path
+  # end
+
 end
 
 private
 def list_params
-  params.require(:list).permit(:title, :body)
+  params.require(:list).permit(:title, :body, :image)
 end
--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c447b8fc3e6b9d5e7d4fa63216a4867d2e90d4f47598b289bd59fe07aa0dc90f47887a9346e6ab77a1eed7b8714d6bb9a8d0cc2f12044a9555b2c86b5181d3ad'

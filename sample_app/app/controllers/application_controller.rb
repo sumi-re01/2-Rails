1d6fd57a849aecd35e603040dad2c2b9286c3fd2
sumi-re01 <owob.viola@gmail.com>
2021-02-02 09:35:29 +0000

    [Finish]2-7

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  Refile.secret_key = '1a183a02341e9b018f7f2d57e37f7c5df58fb898b280f3fce2865ff970cff4242f34c0d56fcb091895ae2694efaf8b23204ea0447c26ae0fd654e230a1f56353'
 end
